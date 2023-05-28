6a6a4d0d1beea1b721b4e08fb306ad10cabe6e82
jabelop <jabelop@hotmail.com>
2023-05-28 03:07:22 -0400

    - añadido el módulo de auth en el módulo de app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from './config/db';
+import { UserRepository } from './auth/domain/UserRepository';
+import { UserRepositoryMysql } from './auth/infraestructure/UserRepositoryMysql';
+import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
+import { DataSource } from 'typeorm';
+
+const userRepositoryProvider = {provide: UserRepository, useClass: UserRepositoryMysql};
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig)],
+  imports: [TypeOrmModule.forRoot(dbConfig), AuthModule],
   controllers: [],
-  providers: [],
+  providers: [JwtService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource){}
+}
