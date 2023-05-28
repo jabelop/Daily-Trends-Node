9c08b78095a2ae20ef6b37d8450e71265c557b7a
jabelop <jabelop@hotmail.com>
2023-05-28 02:55:36 -0400

    - añadidos archivos de configuración de base de datos y JWT

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,25 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
+import * as dotenv from 'dotenv';
+import { UserTypeOrm } from "../auth/infraestructure/UserTypeOrm";
+
+dotenv.config();
+
 const process = require("process");
+
+const host: string = process.env.DB_HOST;
+const port: number = process.env.DB_PORT;
+const username: string = process.env.DB_USER;
+const password: string = process.env.DB_PASSWORD;
+const database: string = process.env.DB_NAME;
 
 export const dbConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: <string>process.env.DB_HOST,
-    port: <number>process.env.DB_PORT,
-    username: <string>process.env.DB_USER,
-    password: <string>process.env.DB_PASSWORD,
-    database: <string>process.env.DB_NAME,
-    entities: [],
+    host,
+    port,
+    username,
+    password,
+    database,
+    entities: [UserTypeOrm],
     synchronize: true,
 };
