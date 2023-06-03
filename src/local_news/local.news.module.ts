9a434056a0177799a2e8d84a08766c167a8ecc3b
jabelop <jabelop@hotmail.com>
2023-06-03 15:42:45 -0400

    - añadido el repositorio y modelo de mongoose en el módulo de noticias locales

--- src/local_news/local.news.module.ts
+++ src/local_news/local.news.module.ts
@@ -8,12 +8,16 @@ import { NewsService } from './application/news.service';
 import { NewRepository } from './domain/NewRepository';
 import { NewRepositoryMysql } from './infraestructure/NewRepositoryMysql';
 import { JwtStrategy } from '../auth/infraestructure/strategies/jwt-strategy';
+import { NewRepositoryMongoose } from './infraestructure/mongoose_repository/NewRepositoryMongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NewMongoose, NewMongooseSchema } from './infraestructure/mongoose_repository/NewMongoose';
 
-const newRepositoryProvider = {provide: NewRepository, useClass: NewRepositoryMysql};
+const newRepositoryProvider = {provide: NewRepository, useClass: NewRepositoryMongoose};//useClass: NewRepositoryMysql};
 
 @Module({
     imports: [ 
-        TypeOrmModule.forFeature([NewTypeOrm]),
+        //TypeOrmModule.forFeature([NewTypeOrm]),
+        MongooseModule.forFeature([{ name: NewMongoose.name, schema: NewMongooseSchema }])
       ],
       controllers: [NewsController],
       providers: [NewsService, newRepositoryProvider, 
